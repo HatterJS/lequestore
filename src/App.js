4a5adad7a3d7afbf0a83114499c7c5b69fd6165d
HatterJS <hatter3ds@gmail.com>
2022-09-18 12:16:05 +0300

    add Address block

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ const arrowSVG = <svg width="12" height="7" viewBox="0 0 12 7">
 
 function App() {
   return (
-    <div className="wrapper">
+    <div>
       <header>
         <div className='topBar'>
           <ul className='topBar__mainMenu'>
@@ -57,7 +57,25 @@ function App() {
             <p>066 0469 277 - Телеграм</p>
             <p>067 9766 777 - Вайбер</p>
           </div>
-          <div className="addressBlock__cartBlock"></div>
+          <div className="addressBlock__cartBlock">
+            <div>
+              <svg width="37" height="51" fill="none">
+                <mask id="path-1-inside-1_34_8" fill="white">
+                  <path d="M0 15C0 14.4477 0.447715 14 1 14H36C36.5523 14 37 14.4477 37 15V46C37 48.7614 34.7614 51 32 51H5C2.23858 51 0 48.7614 0 46V15Z"/>
+                </mask>
+                <path d="M0 15C0 14.4477 0.447715 14 1 14H36C36.5523 14 37 14.4477 37 15V46C37 48.7614 34.7614 51 32 51H5C2.23858 51 0 48.7614 0 46V15Z" fill="#FFE500" stroke="#404040" stroke-width="6" mask="url(#path-1-inside-1_34_8)"/>
+                <path d="M10 18.5V11C10 6.02944 14.0294 2 19 2C23.9706 2 28 6.02944 28 11V18.5" stroke="#404040" stroke-width="3" stroke-linecap="round"/>
+                <line x1="9.5" y1="43.5" x2="28.5" y2="43.5" stroke="#404040" stroke-width="3" stroke-linecap="round"/>
+              </svg>
+              <div className="addressBlock__cartCounter">
+                <p>0</p>
+              </div>
+            </div>
+            <div>
+              <p>0 грн.</p>
+              <a href="./">Оформлення замовлення</a>
+            </div>
+          </div>
         </div>
       </address>
       <nav>
