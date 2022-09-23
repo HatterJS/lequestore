a720cee9554c71ec5a7f0fe60f825949de75a2f5
HatterJS <hatter3ds@gmail.com>
2022-09-23 18:38:03 +0300

    add functionality for addToCartBtn / for cartIcon (open QuickCart) / connected to backend

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import GoodsItem from './components/GoodsItem';
 import Header from './components/Header';
 import QuickCart from './components/QuickCart';
@@ -5,68 +6,28 @@ import Address from './components/Address';
 import NavBar from './components/NavBar';
 import Footer from './components/Footer';
 import './css/App.css';
-import banner from './img/banner-slider/banner_01.jpg';
-import switshot_01 from './img/goods/switshot_01.jpg';
-import switshot_02 from './img/goods/switshot_02.jpg';
-import tshirt_01 from './img/goods/t-shirt_01.jpg';
-import sneakers_001 from './img/goods/sneakers_001.jpg';
-import sneakers_002 from './img/goods/sneakers_002.jpg';
-import sneakers_003 from './img/goods/sneakers_003.jpg';
-import accessory_001 from './img/goods/accessory_001.jpg';
-
-const arrGoods = [
-  {
-    name: 'Худи Balenciaga yellow',
-    cost: '1 250',
-    goodsImage: switshot_01
-  },
-  {
-    name: 'Худи Balenciaga Light Blue',
-    cost: '1 350',
-    goodsImage: switshot_02
-  },
-  {
-    name: 'Футболка FEAR OF GOD ESSENTIALS Los Angeles 3M Boxy Tee White',
-    cost: '700',
-    goodsImage: tshirt_01
-  },
-  {
-    name: 'Кросівки Balenciaga Track Trainer Beige',
-    cost: '2 100',
-    goodsImage: sneakers_001
-  },
-  {
-    name: 'Демісезонні ботинки Black Green на байці',
-    cost: '1 950',
-    goodsImage: sneakers_002
-  },
-  {
-    name: 'Кросівкі adidas Yeezy Boost 350 V2 Ash Stone',
-    cost: '2 599',
-    goodsImage: sneakers_003
-  },
-  {
-    name: 'Шкарпетки FOG ESSENTIALS LOS ANGELES SOCKS Fear of God Kanye West Crew Grey',
-    cost: '249',
-    goodsImage: accessory_001
-  },
-  {
-    name: 'Худи Balenciaga yellow',
-    cost: '1 250',
-    goodsImage: switshot_01
-  }
-];
 
 function App() {
+  const [quickCart, setQuickCart] = React.useState(false);
+  const [arrGoods, setArrGoods] = React.useState([]);
+
+  fetch('https://632db5102cfd5ccc2af512de.mockapi.io/items').then(res => {
+    return res.json();
+  }).then(json => {
+    setArrGoods(json);
+  });
+
   return (
     <div className='wrapper'>
-      <QuickCart />
+      {quickCart && <QuickCart onClose = {() => setQuickCart(false)}/>}
       <Header />
-      <Address />
+      <Address 
+        onClickCart = {() => setQuickCart(true)}
+      />
       <NavBar />
       <div className="content">
         <div className="content__banner-slider">
-          <img src={banner} alt="banner" />
+          <img src="/img/banner-slider/banner_01.jpg" alt="banner" />
           <ul>
             <li></li>
             <li></li>
--- src/components/QuickCart.js
+++ src/components/QuickCart.js
@@ -1,46 +1,49 @@
-import switshot_01 from '../img/goods/switshot_01.jpg';
-import sneakers_003 from '../img/goods/sneakers_003.jpg';
-
 const deleteFromCart = <svg width="40" height="40" viewBox="0 0 40 40" fill="none">
     <rect width="40" height="40" rx="10" fill="white" fill-opacity="0.25"/>
     <rect x="0.5" y="0.5" width="39" height="39" rx="9.5" stroke="black" stroke-opacity="0.3"/>
     <path fill-rule="evenodd" clip-rule="evenodd" d="M23.2504 26.0279L24.3111 27.0885L26.4324 24.9672L25.3717 23.9065L22.6735 21.2083L25.9336 17.9482L26.9942 16.8875L24.8729 14.7662L23.8123 15.8269L20.5522 19.087L17.5936 16.1284L16.5329 15.0677L14.4116 17.189L15.4723 18.2497L18.4308 21.2083L15.327 24.3121L14.2663 25.3728L16.3876 27.4941L17.4483 26.4335L20.5522 23.3296L23.2504 26.0279Z" fill="black" fill-opacity="0.3"/>
 </svg>
 
-function QuickCart() {
-    return(
-        <div className="quickCart" style={{display: 'none'}}>
-        <div className="quickCart__shadow">
-          <div className="quickCart__content">
-            <h4>АКТИВНІ ЗАМОВЛЕННЯ</h4>
-            <div className="quickCart__itemsBlock">
-              <div className="quickCart__item">
-                <div><img src={sneakers_003} alt="sneakers" /></div>
-                <div className='quickCart__description'>
-                  <p>Кросівкі adidas Yeezy Boost 350 V2 Ash Stone</p>
-                  <p>2 599 грн.</p>
-                </div>
-                <div>{deleteFromCart}</div>
+function QuickCart(props) {
+  
+  return(
+      <div className="quickCart">
+      <div className="quickCart__shadow">
+        <div className="quickCart__content">
+          <h4>АКТИВНІ ЗАМОВЛЕННЯ</h4>
+          <div className="quickCart__itemsBlock">
+            <div className="quickCart__item">
+              <div><img src='/img/goods/sneakers_003.jpg' alt="sneakers" /></div>
+              <div className='quickCart__description'>
+                <p>Кросівкі adidas Yeezy Boost 350 V2 Ash Stone</p>
+                <p>2 599 грн.</p>
               </div>
-              <div className="quickCart__item">
-                <div><img src={switshot_01} alt="sneakers" /></div>
-                <div className='quickCart__description'>
-                  <p>Худи Balenciaga yellow</p>
-                  <p>1 250 грн.</p>
-                </div>
-                <div>{deleteFromCart}</div>
+              <div>{deleteFromCart}</div>
+            </div>
+            <div className="quickCart__item">
+              <div><img src='/img/goods/switshot_01.jpg' alt="sneakers" /></div>
+              <div className='quickCart__description'>
+                <p>Худи Balenciaga yellow</p>
+                <p>1 250 грн.</p>
               </div>
+              <div>{deleteFromCart}</div>
             </div>
-            <div className='quickCart__total'>
-              <p>Всього:</p>
-              <p>3 849 грн.</p>
-            </div>
-            <button className='acceptButton'>Оформити замовлення</button>
-            <button className='acceptButton' style={{background: 'none'}}>Продовжити покупки</button>
           </div>
+          <div className='quickCart__total'>
+            <p>Всього:</p>
+            <p>3 849 грн.</p>
+          </div>
+          <button className='acceptButton'>Оформити замовлення</button>
+          <button
+            className='acceptButton'
+            style={{background: 'none'}}
+            onClick={props.onClose}>
+              Продовжити покупки
+          </button>
         </div>
       </div>
-    )
+    </div>
+  )
 }
 
 export default QuickCart;
